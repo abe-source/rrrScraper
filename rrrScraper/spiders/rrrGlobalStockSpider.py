54a2b2cd6cce742f419fc5ad6d27545b06c1b8d0
Arminas B <arminasb7@gmail.com>
2022-10-30 08:40:33 +0200

    splited spiders to differenet class files

--- rrrScraper/spiders/rrrGlobalStockSpider.py
+++ rrrScraper/spiders/rrrGlobalStockSpider.py
@@ -2,7 +2,7 @@ import json
 from select import select
 from requests import request
 import scrapy
-from ..items import categoryStockMercedesItem, rrrGlobalStockItem
+from ..items import rrrGlobalStockItem
 from scrapy.loader import ItemLoader
 import datetime
 
@@ -47,15 +47,3 @@ class rrrGlobalStockSpider(scrapy.Spider):
         il.add_xpath('vw', "//a[contains(text(),'Volkswagen')]/span")
         il.add_value('timeStamp', datetime.datetime.now().isoformat())
         yield il.load_item()
-
-class categoryStockSpider(scrapy.Spider):
-    name = 'rrrCategoryStock'
-    allowed_domains = ['rrr.lt']
-    start_urls = ['https://rrr.lt/lt/get_parts_ajax?q=mercedes']
-    custom_settings = {'ITEM_PIPELINES': {'rrrScraper.pipelines.rrrCategoryStockPipeline': 400}}
-
-    def parse(self, response):
-        data = json.loads(response)
-        # for categories in data['categories']['134']:
-        #     yield request(categories['part_count'], callback=self)
-    
